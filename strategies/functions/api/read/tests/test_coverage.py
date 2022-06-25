a884a8b2281b6600d5e481469cc50f16f4b32a3a
IvanEng2019 <51962986+IvanEng2019@users.noreply.github.com>
2022-06-25 17:23:12 -0300

    add teste

--- strategies/functions/api/read/tests/test_coverage.py
+++ strategies/functions/api/read/tests/test_coverage.py
@@ -8,15 +8,15 @@ from ..app.app import lambda_handler
 from .input_coverage import CoverageInput
 
 
-def test_lambda_handler_x(aws_credentials, dynamodb_strategies, dynamodb_resource, lambda_context):
-    event = CoverageInput.input_x()
+def test_lambda_handler_read_strategy(aws_credentials, dynamodb_strategies, dynamodb_resource, lambda_context):
+    event = CoverageInput.input_read_strategy()
     res = lambda_handler(event, lambda_context)
     print(res)
     assert res["statusCode"] == 200
 
 
-def test_lambda_handler_y(aws_credentials, dynamodb_strategies, dynamodb_resource, lambda_context):
-    event = CoverageInput.input_y()
+def test_lambda_handler_read_shared(aws_credentials, dynamodb_strategies, dynamodb_resource, lambda_context):
+    event = CoverageInput.input_read_shared()
     res = lambda_handler(event, lambda_context)
     print(res)
     assert res["statusCode"] == 200
